47b2b7999fdb798db19d38b6f05a9c7876d2445a
Haider Ahmad <hidarahmadjob@gmail.com>
2022-09-13 22:07:17 +0430

    Desktop Version have been completed with their style for index and about pages.

--- source/js/humbarger.js
+++ source/js/humbarger.js
@@ -6,12 +6,14 @@ humargerBtn.addEventListener('click', () => {
   if (countMenuClick % 2 === 1) {
     changeIcon.classList.add('fa-solid', 'fa-x');
     changeIcon.classList.remove('fas', 'fa-bars');
+    document.getElementById('hide-me').style.display = 'none';
     // show menu
     document.getElementById('nav-menu').style.left = '0%';
     countMenuClick += 1;
   } else {
     changeIcon.classList.add('fas', 'fa-bars');
     changeIcon.classList.remove('fa-solid', 'fa-x');
+    document.getElementById('hide-me').style.display = 'block';
     // show menu
     document.getElementById('nav-menu').style.left = '-100%';
     countMenuClick += 1;
