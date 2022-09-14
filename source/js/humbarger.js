eba81644976cc9e651cb1f3f845008c6edd70824
Haider Ahmad <hidarahmadjob@gmail.com>
2022-09-14 13:47:18 +0430

    Hover effect and more button in index have been added completely.

--- source/js/humbarger.js
+++ source/js/humbarger.js
@@ -1,21 +1,36 @@
 /* eslint-disable linebreak-style */
 const humargerBtn = document.getElementById('show-menu');
 const changeIcon = document.getElementById('change-icon');
+const moreBtn = document.getElementById('more');
 let countMenuClick = 1;
+let countMoreClick = 1;
 humargerBtn.addEventListener('click', () => {
   if (countMenuClick % 2 === 1) {
     changeIcon.classList.add('fa-solid', 'fa-x');
     changeIcon.classList.remove('fas', 'fa-bars');
-    document.getElementById('hide-me').style.display = 'none';
     // show menu
     document.getElementById('nav-menu').style.left = '0%';
     countMenuClick += 1;
+    document.getElementById('hide-me').style.display = 'none';
   } else {
     changeIcon.classList.add('fas', 'fa-bars');
     changeIcon.classList.remove('fa-solid', 'fa-x');
-    document.getElementById('hide-me').style.display = 'block';
     // show menu
     document.getElementById('nav-menu').style.left = '-100%';
     countMenuClick += 1;
+    document.getElementById('hide-me').style.display = 'block';
+  }
+});
+
+// Show the other feature speakers
+moreBtn.addEventListener('click', () => {
+  if (countMoreClick % 2 === 1) {
+    document.getElementById('more').textContent = 'Show Less';
+    document.querySelector('.change-height').style.height = 'auto';
+    countMoreClick += 1;
+  } else {
+    document.getElementById('more').textContent = 'More';
+    document.querySelector('.change-height').style.height = '500px';
+    countMoreClick += 1;
   }
 });
